3e6a6a9a6a1517f46b538694e45d73c1f8e2910f
Greg Linton <lintong@pagodabox.com>
2015-07-03 13:07:14 -0400

    Initial commit, basic mongodb 3.0 server with narc

---
+++ test/run-test.sh
@@ -0,0 +1,9 @@
+# Test mongo after build
+set -e
+docker run --name=mongo-test -d nanobox/mongo
+docker exec -it mongo-test /bin/bash
+curl localhost:5540/hooks/configure -d '{"logtap_uri":"10.0.2.15:6361","uid":"db1"}'
+sleep 2
+sv status db
+exit
+docker kill mongo-test
