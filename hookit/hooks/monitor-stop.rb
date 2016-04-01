ca0140e23c1d7e33089d92fd123da23a91eefbde
Braxton Huggins <braxton@pagodabox.com>
2016-04-01 16:48:37 -0600

    update to use new hooks

--- hookit/hooks/monitor-stop.rb
+++
@@ -1,6 +0,0 @@
-
-service "db" do
-  action :disable
-  only_if { File.exist?('/etc/service/flip/run') }
-  init :runit
-end
--- test/run-test.sh
+++
@@ -1,9 +0,0 @@
-# Test mongo after build
-set -e
-docker run --name=mongo-test -d nanobox/mongo
-docker exec -it mongo-test /bin/bash
-curl localhost:5540/hooks/configure -d '{"logtap_host":"10.0.2.15:6361","uid":"db1"}'
-sleep 2
-sv status db
-exit
-docker kill mongo-test
